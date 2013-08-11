321989e859d6be9ebb5c7db89c6abf16de1d17d5
Stanley Cen <jiecenzhao@gmail.com>
2013-08-10 22:23:35 -0700

    Fix tab indentation

--- src/mem/vmt.cpp
+++ src/mem/vmt.cpp
@@ -16,50 +16,50 @@
 
 namespace embryo
 {
-	unsigned int vmt::countFuncs(void **vmt)
-	{
+    unsigned int vmt::countFuncs(void **vmt)
+    {
         int cnt = -1;
         vm_prot_t prot;
         do {
             cnt++;
             
-        	kern_return_t status = memmgr::getMemoryProtection(vmt[cnt], prot);
-        	if (status)
-        	{
-        		log().error("vm_region() failed");
-        		break;
-        	}
+            kern_return_t status = memmgr::getMemoryProtection(vmt[cnt], prot);
+            if (status)
+            {
+                log().error("vm_region() failed");
+                break;
+            }
         } while (((prot & VM_PROT_EXECUTE) && (prot & VM_PROT_READ)) || // read/exec
                  ((prot & VM_PROT_READ) && (prot & VM_PROT_EXECUTE) && (prot & VM_PROT_WRITE))); // read/write/exec
         
         log().info(format("vfunc count: %d") % cnt);
-		return cnt;
-	}
+        return cnt;
+    }
     
-	int vmt::findFunc(void **vmt, void *func, unsigned int vfuncs)
-	{
-		if (!vfuncs) vfuncs = countFuncs(vmt);
-		for (unsigned int i = 0; i < vfuncs; i++)
-		{
-			if (vmt[i] == func) return i;
-		}
-		return -1;
-	}
+    int vmt::findFunc(void **vmt, void *func, unsigned int vfuncs)
+    {
+        if (!vfuncs) vfuncs = countFuncs(vmt);
+        for (unsigned int i = 0; i < vfuncs; i++)
+        {
+            if (vmt[i] == func) return i;
+        }
+        return -1;
+    }
     
-	vmt::vmt(void *inst, size_t offset, unsigned int vfuncs)
-	{
-		vftable = makeptr<void **>(inst, offset);
-		oldvmt = *vftable;
-		if (!vfuncs) vfuncs = countFuncs(oldvmt);
-		vcount = vfuncs;
-		array = (void **)malloc((vfuncs + 3) * sizeof(void *));
-		array[2] = oldvmt[-1];
-		for (unsigned int i = 0; i < vfuncs; ++i) array[i + 3] = oldvmt[i];
-		*vftable = array + 3;
-	}
-	vmt::~vmt()
-	{
-		if (vftable) unhook();
-		free(array);
-	}
+    vmt::vmt(void *inst, size_t offset, unsigned int vfuncs)
+    {
+        vftable = makeptr<void **>(inst, offset);
+        oldvmt = *vftable;
+        if (!vfuncs) vfuncs = countFuncs(oldvmt);
+        vcount = vfuncs;
+        array = (void **)malloc((vfuncs + 3) * sizeof(void *));
+        array[2] = oldvmt[-1];
+        for (unsigned int i = 0; i < vfuncs; ++i) array[i + 3] = oldvmt[i];
+        *vftable = array + 3;
+    }
+    vmt::~vmt()
+    {
+        if (vftable) unhook();
+        free(array);
+    }
 }
